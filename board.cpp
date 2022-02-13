1e46be4c24417dae4586c5c5222d568a1b2001bb
Patrick Kneschke <patrick.kneschke@gmx.de>
2022-02-13 16:15:01 +0100

    Implement board with zoom based cell drawing.

--- board.cpp
+++ board.cpp
@@ -1,11 +1,28 @@
 
 #include "board.h"
 
+#include <cstdlib>
+
 
 Board::Board(QWidget *parent) :
-	QFrame(parent)
+	QFrame(parent),
+	cells{QVector<QVector<bool>>(boardSize, QVector<bool>(boardSize, false)), QVector<QVector<bool>>(boardSize, QVector<bool>(boardSize, false))},
+	active(0),
+	zoom(1),
+	viewX(0),
+	viewY(0),
+	speed(1)
 {
 	
+	zoom = 8;
+	// test cells
+	srand(42);
+	for(int i=0; i<boardSize; i++) {
+		for(int j=0; j<boardSize; j++) {
+			if(rand() % 100 == 0)
+				cells[active][i][j] = true;
+		}
+	}
 }
 
 
@@ -15,5 +32,20 @@ void Board::mousePressEvent(QMouseEvent *event) {
 
 
 void Board::paintEvent(QPaintEvent *event) {
+	QPainter painter(this);
 	
+	// clear bckground
+	painter.setPen(Qt::white);
+	painter.setBrush(Qt::white);
+	painter.drawRect(0, 0, boardSize, boardSize);
+	
+	// draw cells
+	painter.setPen(Qt::black);
+	painter.setBrush(Qt::black);
+	for(int i=0; i<boardSize/zoom; i++) {
+		for(int j=0; j<boardSize/zoom; j++) {
+			if(cells[active][viewX + i][viewY + j])
+				painter.drawRect(i*zoom, j*zoom, zoom, zoom);
+		}
+	}
 }
--- board.h
+++ board.h
@@ -5,6 +5,7 @@
 #include <QFrame>
 #include <QMouseEvent>
 #include <QPainter>
+#include <QVector>
 #include <QWidget>
 
 
@@ -14,6 +15,25 @@ public:
 	
 	void mousePressEvent(QMouseEvent *event);
 	void paintEvent(QPaintEvent *event) override;
+	
+	
+	static constexpr unsigned int boardSize = 1024;
+	static constexpr unsigned int minViewSize = 32;
+	
+	
+public slots:
+	void toggleOnOff();
+	void zoomIn();
+	void zoomOut();
+	void setSpeed(int s);
+	
+private:
+	QVector<QVector<bool>> cells[2];
+	unsigned int active;
+	unsigned int zoom;
+	unsigned int viewX;
+	unsigned int viewY;
+	unsigned int speed;
 };
 
 
